afd725d2b5a89afc57948889fc23c4b74a600d94
Ritchelle Posadas <ritchelle.posadas@gmail.com>
2018-11-03 01:44:51 -0400

    finished traversals

--- src/Traversals.java
+++ src/Traversals.java
@@ -1,5 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Stack;
+import java.lang.Math;
 public class Traversals {
 
     public static int[] arr = {1,2,3,4,5,6,7,8,9};
@@ -52,21 +53,42 @@ public class Traversals {
     }
 
     public static void inorderIterative(int index) {
-        //Push root and then every node's left child to the stack
+        //Push root to Stack
+        myStack.push(index);
+        //Push all the left children to Stack
         while (hasLeftChild(index)) {
+            index = leftChild(index);
 //            System.out.println("\nPushing to stack my index " + index);
             myStack.push(index);
-            index = leftChild(index);
 
-            //Only happens ONCE when the node does not have a left child but is not popped to stack
-            if (!hasLeftChild(index)) {
-                myStack.push(index);
-//                System.out.println("\nhehPushing to stack my index " + index);
+        }
 
+        //Do inorder on left subtree but keep root unpopped
+        while (myStack.size() != 1) {
+            int node = myStack.pop();
+//            System.out.println("Popped out index " + node);
+            System.out.print(arr[node] + " ");
+            if (hasRightChild(node)) {
+//                System.out.println("Index " + node + " has a right  child and is at popped from index " + rightChild(node));
+                myStack.push(rightChild(node));
             }
         }
 
-        //While popping all the left children (they're also parents) from the stack, print yourself then push right child
+        //Now SOP root
+        index = myStack.pop(); //array is now empty
+        System.out.print(arr[index] + " ");
+
+        //Time to do inorder on right subtree starting from right leaf or root
+        index = rightChild(index);
+        myStack.push(index);
+        while (hasLeftChild(index)) {
+            index = leftChild(index);
+//            System.out.println("\nPushing to stack my index " + index);
+            myStack.push(index);
+
+        }
+
+        //Do inorder on right subtree and pop until empty this time
         while (!myStack.isEmpty()) {
             int node = myStack.pop();
 //            System.out.println("Popped out index " + node);
@@ -100,9 +122,70 @@ public class Traversals {
     }
 
     public static void postorderIterative(int index) {
+        //Push root to stack
+        myStack.push(index);
+//        System.out.println("Pushed " + index);
 
+        //Push all left children to stack
+//        while (hasLeftChild(index)) {
+//            int node = index;
+//            index = rightChild(node);
+//            System.out.println("\nPushing to stack my index " + index);
+//            myStack.push(index);
+//            index = leftChild(node);
+//            System.out.println("\nPushing to stack my index " + index);
+//            myStack.push(index);
+//            //Advance to next subtree
+//            index = leftChild(node);
+//
+//        }
+
+        //Do left tree first by gathering left children
+        while (hasLeftChild(index)) {
+            index = leftChild(index);
+//            System.out.println("\nPushing to stack my index " + index);
+            myStack.push(index);
+
+        }
+
+
+        //Time to pop some left children then right children, don't pop root just yet
+        while (myStack.size() != 1) {
+//        while (!myStack.isEmpty()) {
+            int node = myStack.pop();
+            System.out.print(arr[node] + " ");
+            int parent = parent(node);
+            if (hasRightChild(parent) && parent != 0)
+                System.out.print(arr[rightChild(parent)] + " ");
+
+        }
+
+        //Push right child of root to stack
+        index = 2;
+        myStack.push(2);
+        while (hasLeftChild(index)) {
+            index = leftChild(index);
+//            System.out.println("\nPushing to stack my index " + index);
+            myStack.push(index);
+
+        }
+
+        //Time to pop some left children then right children as well as root
+//        while (myStack.size() != 1) {
+        while (!myStack.isEmpty()) {
+            int node = myStack.pop();
+            System.out.print(arr[node] + " ");
+            int parent = parent(node);
+            if (hasRightChild(parent) && parent != 0)
+                System.out.print(arr[rightChild(parent)] + " ");
+
+        }
+        myStack.clear();
     }
 
+    public static int parent(int index) {
+        return (int) Math.floor((index-1)/2);
+    }
     public static void main(String[] args) {
 
         System.out.print("Recursive inorder: ");
@@ -122,7 +205,7 @@ public class Traversals {
         System.out.print("\nIterative pre-order: ");
         preorderIterative(0);
 
-        System.out.println("\nIterative post-order: ");
+        System.out.print("\nIterative post-order: ");
         postorderIterative(0);
     }
 }
--- src/midtermDriver.java
+++ src/midtermDriver.java
@@ -1,70 +1,17 @@
 import java.util.Stack;
 public class midtermDriver {
 
-    public static void push(String str, String[] arr) {
-        int pushTo = size(arr) + 1;
-        arr[pushTo] = str;
-    }
-
-    public static String pop(String[] arr) {
-        int toPop = size(arr);
-        String temp = arr[toPop];
-        arr[toPop] = "I'M EMPTY";
-        return temp;
-    }
-
-    public static boolean hasLeftChild(int node, String[] arr){
-        if (node*2+1 > size(arr))
-            return false;
-        return true;
-
-    }
-
-    public static int size(String[] arr) {
-        int i = -1;
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i].equals("I'M EMPTY"))
-                break;
-        }
-        return i;
-    }
-
-    public static int leftChild(int index, String[] arr) {
-        int newIndex = 2*index + 1;
-        int size = size(arr);
-        if (newIndex > size)
-            return -1;
-        else
-            return newIndex;
-    }
 
     public static void main(String[] args) {
-        String[] arr = {"A", "B", "C", "D", "E", "F"};
-//        Stack<String> myStack = new Stack<>();
-        String[] myStack = new String[10];
-        for (int i = 0; i < myStack.length; i++) {
-            myStack[i] = "I'M EMPTY";
+        int[] intArr = {1,2,3,4,5,6,7};
+        int[] newArr = new int[intArr.length];
+        int i = 0;
+        int j = intArr.length;
+        for (i = 0, j = intArr.length-1 ; i < intArr.length && j >= 0; i++, j-- ) {
+            newArr[j] = intArr[i];
         }
 
-        String father = arr[0];
-        int fatherIndex = 0;
-
-        while (hasLeftChild(fatherIndex, arr)) {
-            System.out.println("Pushing " + father + " to stack" );
-            push(father, myStack);
-            int newLeftChildIndex = leftChild(fatherIndex, arr);
-            // increment father
-            if (newLeftChildIndex == -1)
-                break;
-            father = arr[newLeftChildIndex];
-            fatherIndex = newLeftChildIndex;
-//            System.out.println(father);
-        }
-
-        for (int i = 0; i < myStack.length; i++) {
-            System.out.println(myStack[i]);
-        }
-
-//        System.out.println(pop(myStack));
+        for (int k = 0; k < newArr.length; k++)
+            System.out.println(newArr[k] + " ");
     }
 }
